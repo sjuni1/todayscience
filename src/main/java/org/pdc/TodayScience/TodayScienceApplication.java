6da324062e0f76face2ba28dfabcd5f671bfedb2
dongcheolpark <head02325@gmail.com>
2021-04-03 21:32:40 +0900

    start

---
+++ src/main/java/org/pdc/TodayScience/TodayScienceApplication.java
@@ -0,0 +1,13 @@
+package org.pdc.TodayScience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayScienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayScienceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pdc/TodayScience/TodayScienceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pdc.TodayScience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayScienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
